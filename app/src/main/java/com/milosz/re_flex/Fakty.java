18774a21a0c2bc145bc8165890a4045d22960633
Miłosz <milosz.gus@gmail.com>
2018-11-19 18:26:50 +0100

    aktywnosc ma wyswietlać fakty dotyczace demencji po ukonczonej grze

---
+++ app/src/main/java/com/milosz/re_flex/Fakty.java
@@ -0,0 +1,14 @@
+package com.milosz.re_flex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fakty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fakty);
+        //aktywnosc ma wyswietlać fakty dotyczace demencji po ukonczonej grze
+    }
+}
